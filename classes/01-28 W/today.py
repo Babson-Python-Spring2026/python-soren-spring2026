789a0f703dd614d9141d9630db07f94ae400237c
student <student@example.com>
2026-02-01 21:27:47 -0500

    Local save before sync (20260201)

---
+++ classes/01-28 W/today.py
@@ -0,0 +1,40 @@
+
+x = 5
+y = x + 3
+
+if x > 0:
+    print('positive')
+    if y > 5:
+        print('bigger')
+
+print('done')
+
+name = ''
+
+if not name:
+    print('check')
+
+if name == False:
+    print('Worked')
+
+
+# GRADER
+grade = 73
+
+if grade >= 90:
+    print('A')
+elif grade >= 80:
+    print('B')
+elif grade >= 70:
+    print('C')
+else:
+    print('D')
+
+x = input('Enter an integer')
+
+x = 5
+y = 7
+x = y
+y = x
+print(x)
+print(y)
